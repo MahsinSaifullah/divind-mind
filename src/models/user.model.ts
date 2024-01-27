d3e2f59586ae9fb26e686a984a0a3fe4da674896
macbookpro <mahsinsaifullah@gmail.com>
2024-01-27 10:13:06 +0000

    feat: add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, select: false },
+  type: { type: String, enum: ['creator', 'player'], required: true },
+  code: { type: String, default: null },
+});
+
+export const userModel = mongoose.model('User', userSchema);
