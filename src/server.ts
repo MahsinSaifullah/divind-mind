93a8630a5e86717b4b52730f527a1ce84a3ac0ce
macbookpro <mahsinsaifullah@gmail.com>
2024-01-27 09:52:36 +0000

    chore: setup database connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -14,3 +15,9 @@ app.use(
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => console.log('Connected to database successfully...'));
+mongoose.connection.on('error', (error: Error) => console.log(error));
