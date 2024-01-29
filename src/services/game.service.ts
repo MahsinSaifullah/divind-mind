0b14bb3e11d8506ca8419c822ccbba321e581619
macbookpro <mahsinsaifullah@gmail.com>
2024-01-29 22:09:49 +0000

    feat: add game service

---
+++ src/services/game.service.ts
@@ -0,0 +1,46 @@
+import { gameModel } from 'models';
+import { IGame, IQuiz } from '../types';
+
+const createGame = async (gameData: IGame) => await gameModel.create(gameData);
+
+const getGameById = async (id: string) => await gameModel.findById(id);
+
+const getGameByCode = async (code: string) => await gameModel.findOne({ code });
+
+const hasGameWithCodeReachedLimit = async (code: string) => {
+  const foundGame = await getGameByCode(code);
+
+  if (!foundGame) return false;
+
+  return foundGame.players.length > foundGame.maxPlayerLimit;
+};
+
+const updateGame = async (id: string, updatedData: IGame) =>
+  await gameModel.findByIdAndUpdate(id, updatedData, { new: true });
+
+const addPlayerToGameWithCode = async (code: string, playerId: string) =>
+  await gameModel.findOneAndUpdate(
+    { code },
+    { $push: { players: playerId } },
+    { new: true }
+  );
+
+const addQuizToGameWithCode = async (code: string, quiz: IQuiz) =>
+  await gameModel.findOneAndUpdate(
+    { code },
+    { $push: { quizes: quiz } },
+    { new: true }
+  );
+
+const deleteGame = async (id: string) => await gameModel.findByIdAndDelete(id);
+
+export const gameService = {
+  createGame,
+  getGameById,
+  getGameByCode,
+  hasGameWithCodeReachedLimit,
+  updateGame,
+  addPlayerToGameWithCode,
+  addQuizToGameWithCode,
+  deleteGame,
+};
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './user.service';
 export * from './auth.service';
 export * from './validation.service';
+export * from './game.service';
