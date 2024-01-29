8d58f9c31583b0771fa1f7098d1fa3062f1f730b
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-01-29 17:44:00 +0000

    feat: add game interface

---
+++ src/types/game.type.ts
@@ -0,0 +1,24 @@
+export interface IAnswer {
+  answer: string;
+  isCorrect: boolean;
+}
+
+export interface IQuestion {
+  question: string;
+  answers: IAnswer[];
+}
+
+export interface IQuiz {
+  id: string;
+  title: string;
+  questions: IQuestion[];
+}
+
+export interface IGame {
+  id: string;
+  code: string;
+  createrId: string;
+  players: string[];
+  maxPlayerLimit: number;
+  quizes: IQuiz[];
+}
