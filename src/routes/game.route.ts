8f14493fd2dc2467df9bab258ef08ed088714157
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-02-01 16:05:25 +0000

    chore: remove empty lines between game routes

--- src/routes/game.route.ts
+++ src/routes/game.route.ts
@@ -7,12 +7,9 @@ const gameRouter = Router();
 
 gameRouter.get('/', middleware.auth('creator'), gameController.getAllGamesForUser)
 gameRouter.get('/:id/player', middleware.auth('creator'), gameController.getAllPlayersOfGame)
-
 gameRouter.post('/', middleware.auth('creator'), gameController.createNewGame);
 gameRouter.post('/:id/quiz', middleware.auth('creator'), gameController.addQuizToGame);
-
 gameRouter.patch('/:id', middleware.auth('creator'), gameController.updateGame)
-
 gameRouter.delete('/:id', middleware.auth('creator'), gameController.deleteGame)
 
 
