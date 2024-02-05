7e1eb27449b2e20396acc7e210d290179bb3e872
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-02-05 11:07:54 +0000

    feat: add types to models

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -1,32 +1,33 @@
 import mongoose from 'mongoose';
 
 import { gameConfig } from '../configs';
+import { IAnswer, IGame, IQuestion, IQuiz } from 'types';
 
-const answerSchema = new mongoose.Schema({
+const answerSchema = new mongoose.Schema<IAnswer>({
   answer: { type: String, required: true },
   isCorrect: { type: Boolean, required: true },
 });
 
-const questionSchema = new mongoose.Schema({
+const questionSchema = new mongoose.Schema<IQuestion>({
   question: { type: String, required: true },
   answers: [answerSchema],
 });
 
-const quizSchema = new mongoose.Schema({
+const quizSchema = new mongoose.Schema<IQuiz>({
   title: { type: String, required: true },
   questions: [questionSchema],
 });
 
-const gameSchema = new mongoose.Schema({
+const gameSchema = new mongoose.Schema<IGame>({
   code: { type: String, required: true },
   creatorId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: 'user',
     required: true,
   },
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+  players: [{ type: String, ref: 'user' }],
   maxPlayerLimit: { type: Number, default: gameConfig.defaultPlayersLimit },
   quizes: [quizSchema],
 });
 
-export const gameModel = mongoose.model('Game', gameSchema);
+export const gameModel = mongoose.model<IGame>('Game', gameSchema);
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import { IUser } from 'types';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUser>({
   username: { type: String, required: true },
   password: { type: String },
   type: { type: String, enum: ['creator', 'player'], required: true },
   code: { type: String, default: null },
 });
 
-export const userModel = mongoose.model('User', userSchema);
+export const userModel = mongoose.model<IUser>('User', userSchema);
